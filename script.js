04bf4a50d0efc7d506d5b6438095a71ed01be1f2
Sirano98 <protsiuk1998@gmail.com>
2021-07-01 15:17:30 +0300

    add: options for fixed header script

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(function () {
 
+    /* Fixed header */
     const header = $("#header");
     const intro = $("#intro");
     let introHeight;
     let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll", function () {
+    $(window).on("scroll load resize", function () {
         scrollPos = $(this).scrollTop();
         introHeight = intro.innerHeight();
 
