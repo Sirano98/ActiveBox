96eea5905f2da4eb6c6207d96894187407f3c2bd
Sirano98 <protsiuk1998@gmail.com>
2021-07-02 00:17:27 +0300

    add: smooth scroll

--- script.js
+++ script.js
@@ -16,4 +16,15 @@ $(function () {
             header.removeClass("fixed");
         }
     })
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        let elementId = $(this).data("scroll");
+        let elementOffset = $(elementId).offset().top;
+
+        $("html").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
 });
